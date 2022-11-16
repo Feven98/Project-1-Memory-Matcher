a18393f0d14a6e212b1c6529657aa32369de8d55
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2022-11-16 14:34:56 -0500

    set timer

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("fighting")
+//console.log("fighting")
+// 
+
+// const score=document.querySelector()
+// const loss=document.querySelector()
+// const win=document.querySelector()
+// set timer to work
+const time=document.querySelector('#timer');
+(function (){
+    let second=0;
+    timer=setInterval(()=>{
+        time.innerHTML='00:'+second;
+        second++;
+}, 2000) // every 2 second
+
+})()
+//const start=document.querySelector()
