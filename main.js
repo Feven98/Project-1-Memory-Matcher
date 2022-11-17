af6a91b0aea3696053b8a8a1c0fff0bb9c60f63f
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2022-11-16 23:06:00 -0500

    declar information on picture and declare variables

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-//console.log("fighting")
-// set timer to work
+// declaring variables
 const time=document.querySelector('.timer');
+const result=document.querySelector('#score');
+const startButton=document.querySelector('#start');
+const restartButton=document.querySelector('#restart');
+const gameContainer=document.querySelector('#gameBox');
+const gameCondition=document.querySelector('#gameCondition');
+let game={
+    action:{
+        cards,
+        interval,
+        firstCard:false,
+        secondCard:false,
+     },
+ }
+ // card images and names
+ const cardType=[   { name: "v", image: "v.png"},
+                    { name: "brown", image: "brown.png"},
+                    { name: "chimmny", image: "chimmny.png"},
+                    { name: "dino", image: "dino.png"},
+                    { name: "jin", image: "jin.png"},
+                    { name: "pink", image: "pink.png"},
+                    { name: "rm", image: "rm.png"},
+                    { name: "xo", image: "xo.png"}
+                ];
