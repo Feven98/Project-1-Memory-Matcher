496ebba7e0ddb8ed27a79f47b50a576247e36f4f
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2022-11-17 01:08:14 -0500

    create the first box with card

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // declaring variables
-const time=document.querySelector('.timer');
+const timeCounter=document.querySelector('#timer');
 const result=document.querySelector('#score');
 const startButton=document.querySelector('#start');
 const restartButton=document.querySelector('#restart');
 const gameContainer=document.querySelector('#gameBox');
 const gameCondition=document.querySelector('#gameCondition');
-let game={
-    action:{
-        cards,
-        interval,
-        firstCard:false,
-        secondCard:false,
-     },
- }
+const box=document.querySelector('.pageSetup')
+console.log(box.innerText);
+// console.log(timeCounter.textContent);
+// let game={
+//     action :{
+//         card,
+//         interval,
+//         firstCard:false,
+//         secondCard:false,
+//      },
+// }
  // card images and names
  const cardType=[   { name: "v", image: "v.png"},
                     { name: "brown", image: "brown.png"},
@@ -21,5 +24,38 @@ let game={
                     { name: "jin", image: "jin.png"},
                     { name: "pink", image: "pink.png"},
                     { name: "rm", image: "rm.png"},
-                    { name: "xo", image: "xo.png"}
+                    { name: "xo", image: "xo.png"},
+                    { name: "v", image: "v.png"},
+                    { name: "brown", image: "brown.png"},
+                    { name: "chimmny", image: "chimmny.png"},
+                    { name: "dino", image: "dino.png"},
+                    { name: "jin", image: "jin.png"},
+                    { name: "pink", image: "pink.png"},
+                    { name: "rm", image: "rm.png"},
+                    { name: "xo", image: "xo.png"},
                 ];
+// initialize the timer
+// const startMinutes=3;
+// let time=startMinutes*60;
+// setInterval(countdown, 1000);
+// function countdown(){
+//     const minutes =Math.floor(time / 60);
+//     let seconds =time % 60;
+//     // seconds=seconds<3 ? '0' + seconds : seconds;
+//     timeCounter.innerHTML= ' ${minutes}: ${seconds}';
+//     time--;
+// }
+
+
+
+
+// Creating the box
+function createBoard(){
+    for(let i=0; i<cardType.length;i++){
+    const cardone=document.createElement('img')
+    cardone.setAttribute('src','image/nature (1).png')
+    cardone.setAttribute('data-id', i)
+box.appendChild(cardone)
+    }
+}
+createBoard()
